b4c443d83b996e4557ef2a7165ad216277184490
Adriano Bendazzoli <159480772+Adrianobendazzoli@users.noreply.github.com>
2025-02-27 08:57:39 -0300

    Add files via upload

---
+++ aula 03/processa1.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $idade = $_POST['cxidade'];
+
+    echo "O seu nome é $nome e sua idade é de $idade anos."
+
+?>
---
+++ aula 03/processaex.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $msg = $_POST['cxmsg'];
+
+    echo "Olá $nome <br>";
+    echo "Sua mensagem : <br>";
+    echo " ' $msg ' "
+   
+
+
+?>
