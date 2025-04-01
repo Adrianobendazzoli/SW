5d00e3615581e265317d791b2ad79e9222f60194
Adrianobendazzoli <adrianobendazzoli911@gmail.com>
2025-04-01 14:36:27 -0300

    exerciciofinalizado

--- aula5/index.php
+++ aula5/index.php
@@ -5,50 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="painel/css/estilo.css">
 </head>
 <body>
- <div class="container">
+ 
+<div class></div>
+
+
+<div class="container">
     <form action="processa.php" method="POST">
+      <h1>LOGIN</h1>
+     
   <!-- Email input -->
   <div data-mdb-input-init class="form-outline mb-4">
     <input type="email" id="form2Example1" class="form-control" name="email" />
-    <label class="form-label" for="form2Example1">Email address</label>
+    <label class="form-label" for="form2Example1">Email </label>
   </div>
 
   <!-- Password input -->
   <div data-mdb-input-init class="form-outline mb-4">
     <input type="password" id="form2Example2" class="form-control" name="senha" />
-    <label class="form-label" for="form2Example2">Password</label>
-  </div>
-
-  <!-- 2 column grid layout for inline styling -->
-  <div class="row mb-4">
-    <div class="col d-flex justify-content-center">
-      <!-- Checkbox -->
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-        <label class="form-check-label" for="form2Example31"> Remember me </label>
-      </div>
-    </div>
-
-    <div class="col">
-      <!-- Simple link -->
-      <a href="#!">Forgot password?</a>
-    </div>
+    <label class="form-label" for="form2Example2">Senha</label>
   </div>
 
   <!-- Submit button -->
    <input type="submit" value="Sing in" class="btn btn-primary btn-block mb-4">
-    
-
-  <!-- Register buttons -->
-  <div class="text-center">
-    <p>Not a member? <a href="#!">Register</a></p>
-    <p>or sign up with:</p>
-    <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-facebook-f"></i>
-    </button>
 
     <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
       <i class="fab fa-google"></i>
@@ -64,5 +46,7 @@
   </div>
     </form>  
  </div>
+
+
 </body>
 </html>
--- aula5/processa.php
+++ aula5/processa.php
@@ -2,11 +2,15 @@
     $email = $_POST["email"];
     $senha = $_POST["senha"];
     
-    if ($email == "dri@email.com" && $senha == "1234"){
+    if ($email == "aluno@email.com" && $senha == "1234"){
+       
         //manda para autorizado.php
-        header('Location: painel/autorizado.html');
+        $nome = "Aluno";
+        header('location: painel/autorizado.php?nome ');
+        
     }else{
+        
         //manda para naoautorizado.php
-        header('Location: painel/naoautorizado.html');
+        header('Location: painel/naoautorizado.php');
     }
 ?>
