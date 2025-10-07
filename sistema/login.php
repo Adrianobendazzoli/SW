67278bf360d227534204a779d8c6465d2d2cbb4d
Adriano Bendazzoli <159480772+Adrianobendazzoli@users.noreply.github.com>
2025-10-07 09:00:08 -0300

    Add files via upload

---
+++ sistema/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="processa_login.php" method="POST">
+        email: <input type="text" name="email" id="">
+        senha: <input type="text" name="senha" id="">
+        <input type="submit" value="Logar">
+    </form>
+</body>
+</html>
